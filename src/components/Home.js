15e3f2dc5a200cafa98b9d9dbc9eae97e6011c1e
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-06 14:53:13 -0400

    projects package.json started, not complete

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hi, my name is Nicole Pingar</h1>
+            <h4>and I am a Full Stack Web Developer.</h4>
+        </div>
+    )
+}
