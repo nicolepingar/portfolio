0a2b596a356dd64ec987cf518cc75824746c9314
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-22 18:19:08 -0500

    port changed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     service: "gmail",
