622bda852c0998a6342f083f8cd8cf25d07e5ad7
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-24 09:49:29 -0500

    took out contact and udpated aboutme

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="work" element={<Work />} />
             <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
+            {/* <Route path="contact" element={<Contact />} /> */}
           </Routes>
 
         </HashRouter>
